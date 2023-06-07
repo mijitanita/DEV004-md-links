9919e1bd74b9890c318b6c149820f9a32121df53
Márquez <amarquezc73@hotmail.com>
2023-06-06 20:00:18 -0500

    adicion de --help

--- cli.js
+++ cli.js
@@ -10,9 +10,10 @@ const cli = () => {
     //inicializo  ruta y optiion
     const rutaDada = argv[2];
     const options = {
-        validate: argv.includes('--validate'),
+        help: argv.includes('--help'),
+        statsAndValidate: argv.includes('--stats') && argv.includes('--validate'),
         stats: argv.includes('--stats'),
-        statsAndValidate: argv.includes('--stats') && argv.includes('--validate')
+        validate: argv.includes('--validate')
 
     };
 
@@ -25,22 +26,31 @@ const cli = () => {
 
                 console.log(chalk.bgYellowBright.red('No hay ruta o Ruta inválida verificar si la ruta es  correcta ej. prueba.txt'));
                 return;
+            } else if (options.help) {
+                console.log(chalk.bold.bgYellowBright.italic.white('\n\n\t\t\t\t\t\tHelp:'));
+                console.log(chalk.bold.greenBright('\n--stats --validate: Para obtener estadísticas que necesiten de los resultados de la validación.'));
+                console.log(chalk.bold.greenBright('\n--stats: Para estadísticas básicas sobre los enlaces'));
+                console.log(chalk.bold.greenBright('\n--validate: Para para comprobar si el enlace funciona o no'));
+                console.log(chalk.yellow('\n\n\t\t\t\t\t----------***---------'));
             }
+
             else if (options.statsAndValidate) {
-                console.log(chalk.bold.bgBlue.italic.white('\nStats & Validate:'));
+                console.log(chalk.bold.bgBlue.italic.white('\n\n\t\t\t\t\tStats & Validate:\n\n\t\t\t\t\t'));
                 console.log(chalk.bold.blue('\nTotal:'), chalk.blue(results.Total));
                 console.log(chalk.bold.yellow('\nUnique:'), chalk.yellow(results.Unique));
                 console.log(chalk.bold.red('\nBroken:'), chalk.red(results.Broken));
+                console.log(chalk.yellow('\n\n\t\t\t\t\t----------***---------'));
             }
 
             else if (options.stats) {
-                console.log(chalk.bold.bgBlue.italic.white('\nStats:'));
+                console.log(chalk.bold.bgBlue.italic.white('\n\n\t\t\t\t\tStats:\n\n\t\t\t\t\t'));
                 console.log(chalk.bold.blue('\nTotal:'), chalk.blue(results.Total));
                 console.log(chalk.bold.yellow('\nUnique:'), chalk.yellow(results.Unique));
+                console.log(chalk.yellow('\n\n\t\t\t\t\t----------***---------'));
             }
 
             else if (options.validate) {
-                console.log(chalk.bold.bgBlue.italic.white('\nLinks Validados:'));
+                console.log(chalk.bold.bgBlue.italic.white('\n\n\t\t\t\t\tLinks Validados:\n\n\t\t\t\t\t'));
                // console.log(results)
                 results.forEach((link) => {
 
@@ -49,17 +59,17 @@ const cli = () => {
                     console.log(chalk.bold('Mensaje:'), link.statusText);
                     console.log(chalk.bold('File:'), link.file);
                     console.log(chalk.bold('Texto:'), link.text);
-                    console.log(chalk.white('---***---'));
+                    console.log(chalk.yellow('\n\n\t\t\t\t\t----------***---------\n\n\t\t\t\t\t'));
                 });
 
             }
             else {
-                console.log(chalk.bold.bgBlue.italic.white('\nLinks Encontrados:'));
+                console.log(chalk.bold.bgBlue.italic.white('\n\n\t\t\t\t\tLinks Encontrados:\n\n\t\t\t\t\t'));
                 results.forEach((link) => {   
                 console.log(chalk.bold('Texto:'), link.text);
                 console.log(chalk.underline(link.url));
                 console.log(chalk.bold('File:'), link.file);
-                console.log(chalk.white('---***---'));
+                console.log(chalk.yellow('\n\n\t\t\t\t\t----------***---------\n\n\t\t\t\t\t'));
                 });
             }
 
